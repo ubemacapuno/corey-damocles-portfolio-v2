ccecabd32ae9a1d33cda376a16149250ee5f57e6
Fast DMS Corey <corey.damocles@fastdms.com>
2024-05-02 14:08:22 -0500

    Update vitest, styling

--- src/lib/components/GitHubRepoDetails.svelte
+++ src/lib/components/GitHubRepoDetails.svelte
@@ -14,32 +14,32 @@
 <div class="modal_headers">
 	<h3>
 		{title}
-		<div class="flex_container">
-			<ToolTip placement="right-end" content="Repository">
-				<a href={repoLink} target="_blank" rel="noreferrer noopener">
-					<svg
-						class="github_icon"
-						fill="var(--grey_7)"
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							d="M12 2C6.477 2 2 6.477 2 12c0 4.418 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.009-.868-.014-1.703-2.782.602-3.369-1.34-3.369-1.34-.455-1.157-1.11-1.465-1.11-1.465-.908-.62.069-.608.069-.608 1.003.07 1.532 1.03 1.532 1.03.891 1.529 2.341 1.089 2.912.833.091-.647.349-1.086.635-1.335-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.03-2.682-.103-.253-.447-1.276.098-2.656 0 0 .84-.269 2.75 1.025A9.564 9.564 0 0112 7.029c.85 0 1.7.114 2.5.334 1.909-1.294 2.747-1.025 2.747-1.025.547 1.38.202 2.403.099 2.656.64.698 1.027 1.59 1.027 2.682 0 3.841-2.337 4.687-4.564 4.934.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.741 0 .267.18.577.688.479C19.138 20.164 22 16.416 22 12c0-5.523-4.477-10-10-10z"
-						/>
-					</svg>
+	</h3>
+</div>
+<div class="flex_container">
+	<ToolTip placement="right-end" content="Repository">
+		<a href={repoLink} target="_blank" rel="noreferrer noopener">
+			<svg
+				class="github_icon"
+				fill="var(--grey_7)"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M12 2C6.477 2 2 6.477 2 12c0 4.418 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.009-.868-.014-1.703-2.782.602-3.369-1.34-3.369-1.34-.455-1.157-1.11-1.465-1.11-1.465-.908-.62.069-.608.069-.608 1.003.07 1.532 1.03 1.532 1.03.891 1.529 2.341 1.089 2.912.833.091-.647.349-1.086.635-1.335-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.03-2.682-.103-.253-.447-1.276.098-2.656 0 0 .84-.269 2.75 1.025A9.564 9.564 0 0112 7.029c.85 0 1.7.114 2.5.334 1.909-1.294 2.747-1.025 2.747-1.025.547 1.38.202 2.403.099 2.656.64.698 1.027 1.59 1.027 2.682 0 3.841-2.337 4.687-4.564 4.934.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.741 0 .267.18.577.688.479C19.138 20.164 22 16.416 22 12c0-5.523-4.477-10-10-10z"
+				/>
+			</svg>
+		</a>
+	</ToolTip>
+	{#if demoLink}
+		<h5>
+			<ToolTip placement="right-end" content="Demo">
+				<a href={demoLink} target="_blank" rel="noreferrer noopener">
+					<Icon name="play_arrow" size="largest" />
 				</a>
 			</ToolTip>
-			{#if demoLink}
-				<h5>
-					<ToolTip placement="right-end" content="Demo">
-						<a href={demoLink} target="_blank" rel="noreferrer noopener">
-							<Icon name="play_arrow" size="largest" />
-						</a>
-					</ToolTip>
-				</h5>
-			{/if}
-		</div>
-	</h3>
+		</h5>
+	{/if}
 </div>
 <Grid cols={[1, 2]}>
 	<div class="modal_content_wrapper">
@@ -122,13 +122,14 @@
 	.flex_container {
 		display: inline-flex;
 		align-items: center;
+		justify-content: space-between;
 		gap: var(--gap_smallest);
-		margin: 0 var(--gap);
+		margin: var(--gap_smallest) 0;
 	}
 
 	.github_icon {
-		width: var(--font_large);
-		height: var(--font_large);
+		width: var(--font_largest);
+		height: var(--font_largest);
 
 		&:hover {
 			transition: var(--transition_speed) ease-in-out;
